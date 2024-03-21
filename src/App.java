3528ac15012756dc3f4cc9dbeb3aee7a0694496b
sniper-feng <fengyukunfyk@sina.com>
2024-03-21 17:13:28 +0800

    Initialization Commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+package src;
+
+/**
+ * @author fengyukun
+ * Created at 21/3/24 17:11
+ * Email : @author fengyukufyk@sina.com
+ * Package : PACKAGE_NAME
+ * @version 1.00.00
+ **/
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Project template");
+    }
+}
