0fb36c21289eb910fbc80ca0fae6107b755e733b
thesmolbeann <134041980+thesmolbeann@users.noreply.github.com>
2024-03-25 23:57:36 +0800

    Update Branch.java

--- src/branch/Branch.java
+++ src/branch/Branch.java
@@ -1,1 +1,18 @@
+package src.branch
 
+public class Branch {
+  private String name;
+  private String location;
+  private int staffQuota;
+  private Staff[] staffList = new Staff[staffQuota];
+
+  public Branch(String name, String location, int staffQuota) {
+    this.name=name;
+    this.location=location;
+    this.staffQuota=staffQuota;
+    for (int i = 1; i < staffQuota; i++) {
+        staffList[i] = new staff('NA', 'NA', 'NA', 'NA', 0, name);
+    }
+  }
+  
+}
