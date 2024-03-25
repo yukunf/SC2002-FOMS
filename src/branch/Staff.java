de647c5482f4bfa7cbc4e9e58be21b90b12fc1a5
thesmolbeann <134041980+thesmolbeann@users.noreply.github.com>
2024-03-25 23:18:27 +0800

    Update Staff.java

--- src/branch/Staff.java
+++ src/branch/Staff.java
@@ -1,12 +1,16 @@
-import src.order.Order
+import src.order.Order;
+import java.util.Scanner;
 
-public class staff {
-	public String name;
-	public String loginID;
-	public char role;
-	public char gender;
-	public int age;
-	public String branch;
+public class Staff {
+	Scanner sc = new Scanner(System.in);
+	private static Order currentOrder = new Order(); // Holds the order currently making ? 
+	
+	private String name;
+	private String loginID;
+	private char role;
+	private char gender;
+	private int age;
+	private String branch;
 	
 	public staff(String name, String loginID, char role, char gender, int age, String branch) {
 		this.name=name;
@@ -17,13 +21,27 @@ public class staff {
 		this.branch=branch;		
 	}
 	
-	public void displayOrder() {
+	public void displayOrders() {
 		
 	}
 	public void viewDetails(Order order) {
-
+		System.out.println("select order to view (input orderID): ");
+		String currentOrder = sc.nextLine();
+		currentOrder = 
 	}
 	public void processOrder() {
-		
+		System.out.println("select order to process (input orderID): ");
+		String currentOrder = sc.nextLine();
+		currentOrder = 	// not sure how to access the order?
+		System.out.println("change status to 1: PREPARING, 2: READY, 3: CANCELLED");
+		int choice = sc.nextInt();
+		switch (choice) {
+			case 1: currentOrder.status = OrderStatus.PREPARING;
+				break;
+			case 2: currentOrder.status = OrderStatus.READY;
+				break;
+			case 3:	currentOrder.status = OrderStatus.CANCELLED;
+				break;
+		}
 	}
 }
