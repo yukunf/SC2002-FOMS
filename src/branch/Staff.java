f8734ed27428d3f1ae4e65f7d41b27df7d6e374c
thesmolbeann <134041980+thesmolbeann@users.noreply.github.com>
2024-03-25 22:23:08 +0800

    Update Staff.java

--- src/branch/Staff.java
+++ src/branch/Staff.java
@@ -1,1 +1,29 @@
+import src.order.Order
 
+public class staff {
+	public String name;
+	public String loginID;
+	public char role;
+	public char gender;
+	public int age;
+	public String branch;
+	
+	public staff(String name, String loginID, char role, char gender, int age, String branch) {
+		this.name=name;
+		this.loginID=loginID;
+		this.role=role;
+		this.gender=gender;
+		this.age=age;
+		this.branch=branch;		
+	}
+	
+	public void displayOrder() {
+		
+	}
+	public void viewDetails(Order order) {
+
+	}
+	public void processOrder() {
+		
+	}
+}
