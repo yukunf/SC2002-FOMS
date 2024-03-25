679e5859cafdf556bbd4b95191da7e102fda556d
thesmolbeann <134041980+thesmolbeann@users.noreply.github.com>
2024-03-26 00:13:32 +0800

    Update Manager.java

--- src/branch/Manager.java
+++ src/branch/Manager.java
@@ -1,6 +1,17 @@
 package src.branch
 
 public class Manager extends Staff {
+  public Manager() {
+
+  }
   
+  public void displayStaff() {
+    for (int i=0; i< ) {
+      System.out.println("")
+    } 
+  }
+  public void editMenu() {
+
+  }
 
 }  
