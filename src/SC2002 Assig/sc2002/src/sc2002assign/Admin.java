c5da4f8f2eab453a2da1ca21d40560168e6a041a
Eva-byf <161175792+Eva-byf@users.noreply.github.com>
2024-03-26 16:21:24 +0800

    Admin.java

--- src/SC2002 Assig/sc2002/src/sc2002assign/Admin.java
+++ src/SC2002 Assig/sc2002/src/sc2002assign/Admin.java
@@ -1,5 +1,50 @@
 package sc2002assign;
+import java.util.Scanner;
 
 public class Admin {
+	Scanner sc = new Scanner(System.in);
+	
+	
+	
+public void AddStaff() {
+		
+}
+public void EditStaff() {
+	
+}
+public void RemoveStaff() {
+	
+}
+public void DisplayStaff() {
+	
+}
+public void AssignManager() {
+	
+}
+public void TransferStaffManager() {
+	
+}
+public void addpaymentmethod() {
+	
+}
+public void removepaymentmethod() {
+	
+}
+public void open() {
+	
+}
+public void close() {
+	
+}
+
+
+
+
+
+
+
+
+
 
 }
+
