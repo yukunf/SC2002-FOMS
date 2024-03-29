31a74235cf4497c10c4deda5c8d249fc83023d6e
gaoxinyuee <88646769+gaoxinyuee@users.noreply.github.com>
2024-03-29 15:05:08 +0800

    Update Payment.java

--- src/order/Payment.java
+++ src/order/Payment.java
@@ -1,14 +1,49 @@
 package src.order;
 
 /**
- * @author fengyukun
+ * @author xinyue
  * Created at 26/3/24 13:24
- * Email : @author fengyukufyk@sina.com
- * Package : src.order
+ * Email: dmsxinyue@gmail.com
+ * Package: src.order
  * @version 1.00.00
- **/
+ */
 public class Payment {
-    // A dummy class to simulate the process of payment
-    //TODO
 
+    private double amount;
+    private String paymentMethod; //eg. credit card, PayPal
+
+
+    public Payment(double amount, String paymentMethod) {
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+    }
+
+    
+    //assuming all payments are successful
+    public void processPayment() {
+        System.out.println("Payment of " + amount + " via " + paymentMethod + " has been processed successfully.");
+    }
+
+    //getters and setters
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    //for testing
+    public static void main(String[] args) {
+        Payment payment = new Payment(100, "Credit Card");
+        payment.processPayment(); 
+    }
 }
