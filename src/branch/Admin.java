4cb0ac170376082cc4f26add9354e6e80f752e03
Eva-byf <BAIY0026@e.ntu.edu.sg>
2024-03-30 18:37:45 +0800

    Update Admin.java

--- src/branch/Admin.java
+++ src/branch/Admin.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Admin {
-
+	List<Branch> branchlist;
 	Scanner sc = new Scanner(System.in);
 
 
@@ -40,13 +40,8 @@ public void AddStaff(Branch branch) {
 public void EditStaff(Staff a,Branch branch) {
 
 	List<Staff> stlist;
+	int ind =branch.getStaffList().indexOf(a);
 
-	for (Staff stf: branch.getStaffList()){
-		if (stf.getStaffName().equals(a.getStaffName())){
-			int ind =branch.getStaffList().indexOf(a);
-			break;
-		}
-	}
 
 	System.out.println("What do you want to edit");
 	System.out.println("1.name");
@@ -58,30 +53,64 @@ public void EditStaff(Staff a,Branch branch) {
 
 	switch (choice) {
 		case 1:
-		System.out.println("key in the new name");
-		String name=sc.nextLine();
-		a.changeStaffName(a,name);
-		stlist=branch.getStaffList().remove(ind);
-		branch.setStaffList(stlist.set(ind, a));
-		return;			
-		break;
-
-		
+			System.out.println("key in the new name");
+			String name=sc.nextLine();
+			a.changeStaffName(a,name);
+			break;
 		case 2:
-		System.out.println("key in the new ID");
-		String id=sc.nextLine();
-		a.changeStaffID(a,id);
+			System.out.println("key in the new ID");
+			String id=sc.nextLine();
+			a.changeStaffID(a,id);
+			break;
 		case 3:
-		System.out.println("key in the new gender");
+			System.out.println("key in the new gender");
+			char gender =sc.nextLine().charAt(0);
+			a.changeStaffgender(a, gender);
+			break;
 		case 4:
-		System.out.println("key in the new age");
+			System.out.println("key in the new age");
+			int age=sc.nextInt();
+			a.changeStaffage(a, age);
+			break;
 		case 5:
-		System.out.println("key in the new branch");
+			System.out.println("key in the new branch");
+			String branchnm=sc.nextLine();
+			a.changeStaffbranch(a, branchnm);
+			stlist=branch.getStaffList();
+			stlist.remove(ind);
+			branch.setStaffList(stlist);
 
+			int branchindex = -1;
+
+			for (int i=0;i<branchlist.size();++i){
+				if (branchlist.get(0).getBranchName().equals(branchnm)){
+					branchindex=i;
+					break;
+				}
+	
+			}
+			if (branchindex==-1) {
+				System.out.println("Branch does not exist.");
+				return;
+			}
+			Branch bh=branchlist.get(branchindex);
+			List<Staff> stflist=bh.getStaffList();
+			stflist.add(a);
+			bh.setStaffList(stflist);
+			return;		
 	}
+	stlist=branch.getStaffList();
+	stlist.remove(ind);
+	stlist.set(ind, a);
+	branch.setStaffList(stlist);
 }
 
-public void RemoveStaff(Staff a) {
+public void RemoveStaff(Staff a,Branch branch) {
+	int index=branch.getStaffList().indexOf(a);
+	List<Staff> stflist=branch.getStaffList();
+	stflist.remove(index);
+	branch.setStaffList(stflist);
+	return;
 	
 }
 public void DisplayStaff() {
