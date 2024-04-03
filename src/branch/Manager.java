e5de746291107190063496f418737a875c966776
thesmolbeann <134041980+thesmolbeann@users.noreply.github.com>
2024-04-03 23:59:43 +0800

    Update Manager.java

--- src/branch/Manager.java
+++ src/branch/Manager.java
@@ -16,28 +16,30 @@ public class Manager extends Staff {
   }
   
   public void displayStaff() {
-	int StaffQuota=getStaffQuota();
-    for (int i=0; i<StaffQuota; i++) {
-      System.out.println(staffList[i]);
-    }
+	for(Staff staff : staffList){
+		System.out.println(staff.getStaffName());
+	}
   }
+	
   public void editMenu() {
 	  System.out.println("Select action: 1. Add 2. Edit 3. Remove");
+	  int choice = sc.nextInt();
 	  switch (choice) {
 	  case 1: System.out.println("Select new item category: 1. Side 2. Set Meal 3. Burger 4. Drink");
-		int category = sc.nextInt();
-		switch (category) {
-			case 1: FoodCategory category = SIDE;
+		int answer = sc.nextInt();
+		FoodCategory category = null;
+		switch (answer) {
+			case 1: category = SIDE;
 				break;
-			case 2: FoodCategory category = SET_MEAL;
+			case 2: category = SET_MEAL;
 				break;
-			case 3: FoodCategory category = BURGER;
+			case 3: category = BURGER;
 				break;
-			case 4: FoodCategory category = DRINK;
+			case 4: category = DRINK;
 				break;
 		}
 		System.out.println("Enter new item name: ");
-	    String name = sc.nextLine();
+	    	String name = sc.nextLine();
 		System.out.println("Enter new price: $");
 	  	double price = sc.nextDouble();
 		System.out.println("Enter branch (NTU/ JP/ JE): ");
@@ -47,37 +49,44 @@ public class Manager extends Staff {
 	  	break;
 	  case 2: System.out.println("Enter item name: ");
 	      String item = sc.nextLine();
-	      int x=0;
-	      while (foodList[x].name != item) {
-	    	  x++;
-	      }
-	      System.out.println(foodList[x].name + "- Price: $" +foodList[x].price+ ", Available: "foodList[x].availability);
-	      System.out.println("Enter new price: ");
-	      double newPrice = sc.nextDouble();
-	      System.out.println("Still available (true/false): ");
-	      boolean newAvail = sc.nextBoolean();
-	      foodList[x].price = newPrice;
-	      foodList[x].availability = newAvail;
-	      System.out.println("Updated to:");
-	      System.out.println(foodList[x].name + "- Price: $" +foodList[x].price+ ", Available: "foodList[x].availability);
-		  break;
-	  case 3: System.out.println("Enter item name: ");
-	  	  String toRemove = sc.nextLine();
-	  	  while (foodList[x] != null) {
-	    	  if (foodList[x].name != toRemove) {
-	    		  x++;
-	    	  }
-	  		  if (foodList[x].name == toRemove) {
-	  			  int y=x+1;
-	  			  while (foodList[y] != null) {
-	  				  foodList[x] = foodList[y];
-	  				  x++;
-	  				  y++;
-	  			  }
-	  			  System.out.println("Removed item.");
+	      int x;
+	      for (x=0; x<foodList.size(); x++) {
+	  		  if (foodList.get(x).getName() == item) {
 	  			  break;
 	  		  }
 	      }
+	      String availability;
+	      if (foodList.get(x).isAvailability()) {
+	    	  availability="Yes";
+	      }
+	      else {
+	    	  availability="No";
+	      }
+	      System.out.println(foodList.get(x).getName() + "- Price: $" +foodList.get(x).getPrice()+ ", Available: " +availability);
+	      System.out.println("Enter new price: ");
+	      double newPrice = sc.nextDouble();
+	      foodList.get(x).setPrice(newPrice);
+	      System.out.println("Still available (true/false): ");
+	      boolean newAvail = sc.nextBoolean();
+	      foodList.get(x).setAvailability(newAvail);
+	      if (foodList.get(x).isAvailability()) {
+	    	  availability="Yes";
+	      }
+	      else {
+	    	  availability="No";
+	      }
+	      System.out.println("Updated to:");
+	      System.out.println(foodList.get(x).getName() + "- Price: $" +foodList.get(x).getPrice()+ ", Available: "+availability);
+		  break;
+	  case 3: System.out.println("Enter item name: ");
+	  	  String toRemove = sc.nextLine();
+	  	  for (int y=0; y<foodList.size(); y++) {
+	  		  if (foodList.get(y).getName() != toRemove) {
+	  			foodList.remove(y);
+	  			System.out.println("Removed item.");
+	  			break;
+	  		  }
+	  	  }	  	  
 		  break;
 	  }
   }
