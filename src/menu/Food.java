c812f6ba5d981fd1a56ab9b39ed59524ec19db76
sniper-feng <fengyukunfyk@sina.com>
2024-04-19 14:23:00 +0800

    JavaDoc For Menu and Food

--- src/menu/Food.java
+++ src/menu/Food.java
@@ -3,12 +3,11 @@ package src.menu;
 import java.io.Serializable;
 
 /**
- * @author fengyukun
- * Created at 21/3/24 17:28
- * Email : @author fengyukufyk@sina.com
- * Package : src
+ * The Food Type. Represents a menu entry.
+ *
+ * @author fengyukun  Created at 21/3/24 17:28 Email : @author fengyukufyk@sina.com Package : src
  * @version 1.00.00
- **/
+ */
 public class Food implements Serializable {
 
     private int ID; //Need ID?
@@ -19,6 +18,15 @@ public class Food implements Serializable {
 
     private boolean availability;
 
+    /**
+     * Instantiates a new Food.
+     *
+     * @param name         the name
+     * @param price        the price
+     * @param branch       the branch
+     * @param category     the category
+     * @param availability the availability
+     */
     public Food(String name, double price, String branch, FoodCategory category, boolean availability) {
         this.name = name;
         this.price = price;
@@ -27,46 +35,102 @@ public class Food implements Serializable {
         this.availability = availability;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param ID the id
+     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    /**
+     * Gets branch it belongs to
+     *
+     * @return the branch
+     */
     public String getBranch() {
     	return branch;
     }
-    
+
+    /**
+     * Gets price.
+     *
+     * @return the price
+     */
     public double getPrice() {
     	return price;
     }
-    
+
+    /**
+     * Sets price.
+     *
+     * @param price the price
+     */
     public void setPrice(double price) {
     	this.price = price;
     }
-    
+
+    /**
+     * Gets category.
+     *
+     * @return the category
+     */
     public FoodCategory getCategory() {
         return category;
     }
 
+    /**
+     * Sets category.
+     *
+     * @param category the category
+     */
     public void setCategory(FoodCategory category) {
         this.category = category;
     }
 
+    /**
+     * Get the boolean indicates is it available
+     * is
+     *
+     * @return the boolean
+     */
     public boolean isAvailability() {
         return availability;
     }
 
+    /**
+     * Sets availability.
+     *
+     * @param availability the availability
+     */
     public void setAvailability(boolean availability) {
         this.availability = availability;
     }
--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -4,27 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author fengyukun
- * Created at 21/3/24 17:40
- * Email : @author fengyukufyk@sina.com
- * Package : src
+ * The type Menu.
+ *
+ * @author fengyukun  Created at 21/3/24 17:40 Email : @author fengyukufyk@sina.com Package : src
  * @version 1.00.00
- **/
+ */
 public class Menu {
     List<Food> foodList;
 
+    /**
+     * Instantiates a new Menu.
+     */
     public Menu() {
         this.foodList = new ArrayList<>();
     }
 
+    /**
+     * Instantiates a new Menu.
+     *
+     * @param foodList the food list
+     */
     public Menu(List<Food> foodList) {
         this.foodList = foodList;
     }
 
+    /**
+     * Get food list list.
+     *
+     * @return the list
+     */
     public List<Food> getFoodList(){
         return foodList;
     }
-    
+
+    /**
+     * Get food list by branch list.
+     *
+     * @param branch the branch
+     * @return the list
+     */
     public List<Food> getFoodListByBranch(String branch){
     	List<Food> branchMenu = new ArrayList<Food>();
     	for(Food f : foodList) {
@@ -34,12 +52,24 @@ public class Menu {
     	}
     	return branchMenu;
     }
-    
+
+    /**
+     * Set food list by branch.
+     *
+     * @param foodList the food list
+     */
     public void setFoodListByBranch(List<Food> foodList){
     	this.foodList = foodList;
     }
 
-    
+
+    /**
+     * Get food list by category list.
+     *
+     * @param branchMenu the branch menu
+     * @param category   the category
+     * @return the list
+     */
     public List<Food> getFoodListByCategory(List<Food> branchMenu, FoodCategory category){
         List<Food> cate = new ArrayList<>();
         for(Food f: branchMenu){
